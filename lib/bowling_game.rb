2fa559d05796e3cc9409c0494f66be1080098aa2
Anna <yedennek@googlemail.com>
2013-11-25 16:31:19 +0000

    Refactored to keep track of frames

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -3,6 +3,7 @@ class Game
   def initialize
     @score = 0
     @rolls = []
+    @frames = []
   end
 
   def roll pins
@@ -10,7 +11,17 @@ class Game
   end
 
   def score
-    @rolls.inject(:+)
+    convert_rolls_to_frames
+    @frames.each { |x|
+      @frame_score =  x.inject(:+)
+      @score += @frame_score
+    }
+    @score
   end
 
+  def convert_rolls_to_frames
+    (0..19).reject(&:odd?).each { |x|
+      @frames << @rolls.slice(x, 2)
+    }
+  end
 end
